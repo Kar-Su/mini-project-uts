cc04386ec5517a25062d45ab910d979404c4d35a
Kar <c030324037@mahasiswa.poliban.ac.id>
2025-05-08 18:58:19 +0800

    Mengoptimalkan fungsi InsertData agar data bersifat dynamic, user dapat melanjutkan insert dari data yang sebelumnya sudah di insert, dan menampilkan data sebelum user melakukan insert data

--- menu.cpp
+++ menu.cpp
@@ -1,77 +1,18 @@
 #include <bits/stdc++.h>
-// #include <iostream>
-// #include <limits>
 
-void dMenu() {
-  system("clear");
-  std::cout << "Aplikasi Sorting Insertion" << "\n";
-  std::cout << "1. Memasukkan Data" << "\n";
-  std::cout << "2. Menampilkan Data" << "\n";
-  std::cout << "3. Sorting" << "\n";
-  std::cout << "4. Kata-Kata" << "\n";
-  std::cout << "5. Exit" << "\n";
-  std::cout << "Masukan angka: ";
-}
-
-void SepatahKata() {
-  system("clear");
-  std::cout << "Hello World " << std::endl;
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  std::cin.get();
-}
-
-template <typename T> void InsertData(T &data) {
-  system("clear");
-  if (!data.empty()) {
-    data.clear();
-  }
-  size_t n;
-
-  std::cout << "Masukkan banyak data yang mau dimasukkan: ";
-  std::cin >> n;
-
-  for (size_t i = 0; i < n; i++) {
-    std::cout << "Masukkan Data[" << i << "]: ";
-    int buff;
-    std::cin >> buff;
-    data.push_back(buff);
-  }
-}
-
-template <typename T> void PrintData(T &data) {
-  system("clear");
-  std::cout << "Array: ";
-  for (const auto &i : data) {
-
-    std::cout << i << " ";
-  }
-
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  std::cin.get();
-}
-
-template <typename T> void SortingData(T &data) {
-  system("clear");
-
-  size_t n = data.size();
-  for (size_t i = 1; i < n; i++) {
-    int tmp = data[i];
-    size_t j = i;
-
-    while (j > 0 and data[j - 1] > tmp) {
-      data[j] = data[j - 1];
-
-      j--;
-    }
-    data[j] = tmp;
-  }
-
-  std::cout << "Sorting data berhasil!" << std::endl;
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  std::cin.get();
-}
 
 using myArray = std::vector<int>;
+
+void dMenu();
+
+template <typename T> void InsertData(T &data);
+
+template <typename T> void PrintData(T &data);
+
+template <typename T> void SortingData(T &data);
+
+void SepatahKata();
+
 
 int main() {
   myArray arr;
@@ -111,4 +52,93 @@ int main() {
   } while (pl != '5');
 
   return 0;
+}
+
+
+void dMenu() {
+  system("clear");
+  std::cout << "Aplikasi Sorting Insertion" << "\n";
+  std::cout << "1. Memasukkan Data" << "\n";
+  std::cout << "2. Menampilkan Data" << "\n";
+  std::cout << "3. Sorting" << "\n";
+  std::cout << "4. Kata-Kata" << "\n";
+  std::cout << "5. Exit" << "\n";
+  std::cout << "Masukan angka: ";
+}
+
+template <typename T> void InsertData(T &data) {
+  PrintData<myArray>(data);
+
+  // if (!data.empty()) {
+  //   data.clear();
+  // }
+  std::string buff;
+  size_t i = data.size();
+  std::cout << std::endl << "Silahkan masukkan data (tekan enter tanpa mengisi data untuk mengakhiri)" << std::endl;
+
+  do {
+    std::cout << "Masukkan Data[" << i << "]: ";
+    std::getline(std::cin, buff);
+
+    if (buff.empty()) {
+      return;
+    }
+
+    data.push_back(stoi(buff));
+    i++;
+  } while (1);
+}
+
+template <typename T> void PrintData(T &data) {
+  system("clear");
+
+  if(data.empty()) {
+    std::cout << "Data anda kosong!";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+    return;
+  }
+
+  std::cout << "Array: ";
+  for (const auto &i : data) {
+    std::cout << i << "  ";
+  }
+
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cin.get();
+}
+
+template <typename T> void SortingData(T &data) {
+  system("clear");
+
+  if(data.empty()){
+    std::cout << "Data anda kosong, silahkan isi terlebih dahulu!" <<std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+    return;
+  }
+
+  size_t n = data.size();
+  for (size_t i = 1; i < n; i++) {
+    int tmp = data[i];
+    size_t j = i;
+
+    while (j > 0 and data[j - 1] > tmp) {
+      data[j] = data[j - 1];
+
+      j--;
+    }
+    data[j] = tmp;
+  }
+
+  std::cout << "Sorting data berhasil!" << std::endl;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cin.get();
+}
+
+void SepatahKata() {
+  system("clear");
+  std::cout << "Hello World " << std::endl;
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cin.get();
 }
